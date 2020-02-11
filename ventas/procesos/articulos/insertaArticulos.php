9cd4b92556fd767384601439e905b4ed1fca19a8
Humberto Israel Perez Rodriguez <humberto.rodriguez@itexico.com>
2020-02-10 20:39:19 -0600

    update

--- ventas/procesos/articulos/insertaArticulos.php
+++ ventas/procesos/articulos/insertaArticulos.php
@@ -21,19 +21,19 @@
 
   if(move_uploaded_file($rutaAlmacenamiento, $rutaFinal)){
     $idimagen=$obj->agregaImagen($datosImg);
-  }
 
-  if($idimagen > 0){
-    $datos[0]=$_POST['categoriaSelect'];
-    $datos[1]=$idimagen;
-    $datos[2]=$iduser;
-    $datos[3]=$_POST['nombre'];
-    $datos[4]=$_POST['descripcion'];
-    $datos[5]=$_POST['cantidad'];
-    $datos[6]=$_POST['precio'];
-    echo $obj->insertaArticulo($datos);
-  } else {
-    echo 0;
+    if($idimagen > 0){
+      $datos[0]=$_POST['categoriaSelect'];
+      $datos[1]=$idimagen;
+      $datos[2]=$iduser;
+      $datos[3]=$_POST['nombre'];
+      $datos[4]=$_POST['descripcion'];
+      $datos[5]=$_POST['cantidad'];
+      $datos[6]=$_POST['precio'];
+      echo $obj->insertaArticulo($datos);
+    } else {
+      echo 0;
+    }
   }
 
 ?>
