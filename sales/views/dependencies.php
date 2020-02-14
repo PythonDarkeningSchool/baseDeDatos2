f61f6621d749451c8205ec888914f7cbd5923eef
Humberto Israel Perez Rodriguez <humberto.rodriguez@itexico.com>
2020-02-13 19:44:38 -0600

    remove sales folder

--- sales/views/dependencies.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" type="text/css" href="../libraries/alertifyjs/css/alertify.css">
-<link rel="stylesheet" type="text/css" href="../libraries/alertifyjs/css/themes/default.css">
-<link rel="stylesheet" type="text/css" href="../libraries/bootstrap-4.4.1-dist/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="../libraries/select2/dist/css/select2.css">
-<script src="../libraries/jquery-3.4.1.min.js"></script>
-<script src="../libraries/alertifyjs/alertify.js"></script>
-<script src="../libraries/bootstrap-4.4.1-dist/js/bootstrap.js"></script>
-<script src="../libraries/select2/dist/js/select2.js"></script>
